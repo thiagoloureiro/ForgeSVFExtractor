b3647e95596528ac7dc4e5ff14fc6caa0649667b
EUROPE\PLTHLOU <thiago.loureiro-de-azevedo@pl.abb.com>
2022-10-07 09:09:24 +0200

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUrn = new System.Windows.Forms.TextBox();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Extract";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // txtUrn
+            // 
+            this.txtUrn.Location = new System.Drawing.Point(109, 23);
+            this.txtUrn.Name = "txtUrn";
+            this.txtUrn.Size = new System.Drawing.Size(570, 22);
+            this.txtUrn.TabIndex = 1;
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(109, 67);
+            this.txtToken.Multiline = true;
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(570, 123);
+            this.txtToken.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Urn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Token";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 291);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.txtUrn);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUrn;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.bucket.manager.ForgeUtils;
+using static WindowsFormsApp1.bucket.manager.ForgeUtils.Derivatives;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            //try
+            // {
+            //var x = await Derivatives.ExtractSVFAsync("", "eyJhbGciOiJSUzI1NiIsImtpZCI6IlU3c0dGRldUTzlBekNhSzBqZURRM2dQZXBURVdWN2VhIn0.eyJzY29wZSI6WyJkYXRhOndyaXRlIiwiZGF0YTpyZWFkIiwiYnVja2V0OmNyZWF0ZSIsImJ1Y2tldDpkZWxldGUiLCJidWNrZXQ6cmVhZCIsImRhdGE6c2VhcmNoIiwidmlld2FibGVzOnJlYWQiXSwiY2xpZW50X2lkIjoiYkpyc3FndkczQTd1VTdiRmtxRDRVZUVYUm0zMUQ2eUciLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbS9hdWQvYWp3dGV4cDYwIiwianRpIjoiUlFJajBrZFd0aVcyMlBUTjV6a2NTRmpuQzFOdUtNTHljclFZM0dUdm8zY0V6ejVSMWtSTFRiNk95TXpTQUoxRCIsImV4cCI6MTY2NTA3NzAyOH0.cyvnauBn_tkKuQNdMBp7-DWHjpa3rJg2f72ANqZu2_uVUjcWnPXM64MbkVcpu9g9Jd6EuKXcWkc7ot1G65KSnOMYyYEYmIG1hkX2fmbqd_GzAXOo9EO1TpgxOqxD-joHZXL-uoot2x0z7BOxR6a6UwaRgkvC3eJ2XJbiUp8FFQaBLLBf6-OTJiNFIAK5WIr_BqZbHOGhe4jdroegdJWb8XbrKSQQ2kHOKhCp3KNJMKQzEK3XxaBR9BHH5e5WDOU3kau5fNz8mvpIXx7DC4QyaqqdUIwuyCxJKFRGDtwA7D_YvB4pP7IJg8CGqvYsRwVw2atLaG_Bs9BMbeompo5eVg");
+            var urn = txtUrn.Text;
+            var AccessToken = txtToken.Text;
+
+            // get the list of resources to download
+            List<Resource> resourcesToDownload = await ExtractSVFAsync(urn, AccessToken);
+
+            var folderPath = $"c:\\tempforge\\{urn}";
+            Directory.CreateDirectory(folderPath);
+
+            var client = new RestClient("https://developer.api.autodesk.com/");
+            foreach (bucket.manager.ForgeUtils.Derivatives.Resource resource in resourcesToDownload)
+            {
+                // prepare the GET to download the file
+                RestRequest request = new RestRequest(resource.RemotePath, Method.Get);
+                request.AddHeader("Authorization", "Bearer " + AccessToken);
+                request.AddHeader("Accept-Encoding", "gzip, deflate");
+                var response = await client.ExecuteAsync(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    // combine with selected local path
+                    string pathToSave = Path.Combine(folderPath, resource.LocalPath);
+                    // ensure local dir exists
+                    Directory.CreateDirectory(Path.GetDirectoryName(pathToSave));
+                    // save file
+                    File.WriteAllBytes(pathToSave, response.RawBytes);
+                }
+                else
+                {
+                    // something went wrong for this file...
+                }
+            }
+
+            MessageBox.Show("Process finished");
+            // }
+            // catch (Exception err)
+            //{
+            //    MessageBox.Show(err.Message);
+            // }
+        }
+    }
+}
